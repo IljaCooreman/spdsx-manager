e5d0a519a4080f4dd9516ee6a05794ca6a264420
Ilja Cooreman <ilja.cooreman@imec.be>
2020-06-21 13:50:48 +0200

    basic wave filtering

--- src/renderer/components/WaveManager/WaveList.tsx
+++ src/renderer/components/WaveManager/WaveList.tsx
@@ -3,16 +3,29 @@ import { useStoreon } from 'storeon/react';
 import Button from '@material-ui/core/Button';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+import { TextField } from '@material-ui/core';
 import { State, WaveManagerEvents } from '../../store/types/types';
 import DraggableAudio from '../DraggableAudio';
 import { GeneralContainerStyle } from '../../styling';
+import DeviceWave, { DndObject } from '../../../classes/DeviceWave';
 
 const Container = styled.div`
     ${GeneralContainerStyle()}
 `;
 
 const WaveList: React.FunctionComponent = () => {
+    const [filterValue, setFilterValue] = React.useState<string>('');
+    const [filteredList, setFilteredList] = React.useState<DndObject<DeviceWave>[]>([]);
     const { dndDeviceWaves } = useStoreon<State, WaveManagerEvents>('dndDeviceWaves');
+
+    React.useEffect(() => {
+        if (filterValue.length <= 0) {
+            setFilteredList(dndDeviceWaves);
+        } else {
+            const regex = new RegExp(filterValue, 'ig');
+            setFilteredList(dndDeviceWaves.filter(wave => regex.test(wave.item.name)));
+        }
+    }, [filterValue, dndDeviceWaves]);
 
     const getListStyle = () => {
         return {
@@ -28,11 +41,17 @@ const WaveList: React.FunctionComponent = () => {
     return (
         <Container>
             <h3>Files on device</h3>
+            <TextField
+                label="Filter"
+                size="small"
+                value={filterValue}
+                onChange={e => setFilterValue(e.target.value)}
+            />
 
             <Droppable droppableId="list-wave" type="PAD" isDropDisabled={true}>
                 {(provided, snapshot) => (
                     <ul style={getListStyle()} ref={provided.innerRef}>
-                        {dndDeviceWaves.map((item, index) => (
+                        {filteredList.map((item, index) => (
                             <DraggableAudio
                                 key={item.id}
                                 dndObject={item}
